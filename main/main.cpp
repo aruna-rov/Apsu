cf53db5916fbac7223c8a809a822186b50b5fe3a
Noeël <noeelmoeskops@hotmail.com>
2019-07-08 16:23:11 +0200

    New Aruna version

--- main/main.cpp
+++ main/main.cpp
@@ -3,7 +3,14 @@
 //
 
 #include <esp_log.h>
+#include <aruna.h>
+using namespace aruna;
+drivers::comm::CommDriver* uart_driver;
 
 extern "C" void app_main(void) {
+    aruna::blinky::set_led(aruna::blinky::LED_ON);
+    uart_driver = new drivers::comm::UART();
+    comm::register_candidate_driver(uart_driver);
+    comm::start();
     ESP_LOGI("MAIN", "hello world!");
 }
