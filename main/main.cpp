a369c9e38fb9e1b359170ab7775f48bc9ee5c6be
Noeël <noeelmoeskops@hotmail.com>
2019-07-17 18:23:04 +0200

    updated aruna lib

--- main/main.cpp
+++ main/main.cpp
@@ -8,6 +8,7 @@ using namespace aruna;
 drivers::comm::CommDriver* uart_driver;
 
 extern "C" void app_main(void) {
+    xTaskCreate(blinky::start_blinky_task, "blink", 2000, NULL, NULL, NULL);
     aruna::blinky::set_led(aruna::blinky::LED_ON);
     uart_driver = new drivers::comm::UART();
     comm::register_candidate_driver(uart_driver);
