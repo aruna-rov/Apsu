f3ce20c396b829ee5bede0a676d74e52614b0fe2
Noeël Moeskops <noeelmoeskops@Hotmail.com>
2020-08-21 13:02:52 +0200

    Apsu map

--- main/component.mk
+++ main/component.mk
@@ -3,4 +3,4 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 COMPONENT_SRCDIRS = .
-COMPONENT_ADD_INCLUDEDIRS = include ../libs/Aruna/include
+COMPONENT_ADD_INCLUDEDIRS = include ../libs/Aruna/include ./include
---
+++ main/include/apsu_map.h
@@ -0,0 +1,37 @@
+//
+// Created by noeel on 21-08-20.
+//
+
+#ifndef APSU_MAP_H
+#define APSU_MAP_H
+
+enum class I2C_address: uint8_t {
+    ADS1015     = 0b1001000,
+    LSM6DSOX    = 0b1101010,
+    BME280      = 0b1110110,
+    IIS2MDL     = 0b0011110,
+    PCA9685     = 0b1000000,
+    INA219      = 0b1000111,
+};
+
+enum class IO {
+    ESP_TXD     = 1,
+    ESP_RXD     = 3,
+    SPI_MISO    = 4,
+    SPI_SCLK    = 5,
+    I2C_SDA     = 16,
+    I2C_SCL     = 17,
+    RS485_RTS   = 18,
+    DS18B20     = 19,
+    SPI_SS0     = 21,
+    RS485_RX    = 22,
+    RS485_TX    = 23,
+    SPI_SS1     = 25,
+    SPI_SS2     = 26,
+    BLDC_M3     = 27,
+    BLDC_RIGHT_Z= 32,
+    BLDC_LEFT_Z = 33,
+    SPI_MOSI    = 34,
+    BLDC_M4     = 35,
+};
+#endif //APSU_MAP_H
